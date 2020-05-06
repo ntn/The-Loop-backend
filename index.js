e92f56c80c29cd0d6c2459300d0428c22db88110
ntn <neethan.bala@gmail.com>
2020-05-06 14:24:04 -0400

    Add basic express server

---
+++ index.js
@@ -0,0 +1,11 @@
+var express = require('express')
+var app = express()
+const port = 3000
+
+app.get('/', function (req, res) {
+  res.send('hello world')
+})
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})
