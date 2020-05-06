cea26a42d79f26fecfa8437ccc8776b4c72fa459
ntn <neethan.bala@gmail.com>
2020-05-06 14:38:48 -0400

    Add eslint into project

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-var express = require('express')
-var app = express()
-const port = 3000
+const express = require('express');
 
-app.get('/', function (req, res) {
-  res.send('hello world')
-})
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
-})
+  console.log(`Listening on port ${port}`);
+});
