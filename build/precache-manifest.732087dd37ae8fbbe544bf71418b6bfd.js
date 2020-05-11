892fb7178369a6b6d71f5c6a7406efe6a1e465f8
ntn <neethan.bala@gmail.com>
2020-05-10 23:52:30 -0400

    uibuild

---
+++ build/precache-manifest.732087dd37ae8fbbe544bf71418b6bfd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0f06760603d667f32fd75fe06a35ac3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac7b25c600662c93ce74",
+    "url": "/static/js/2.473e123e.chunk.js"
+  },
+  {
+    "revision": "3be25bf51018a491ef7f73dfd2dd74c4",
+    "url": "/static/js/2.473e123e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9bccefbdfe6cc303a40",
+    "url": "/static/js/main.fc9eeff1.chunk.js"
+  },
+  {
+    "revision": "dcb750baaa08b5b9e64a",
+    "url": "/static/js/runtime-main.b852ea96.js"
+  }
+]);
